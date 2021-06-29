e44dd03931f8adda9b53294fa194a539364234bf
刘德 <448206058@qq.com>
2021-06-29 22:45:51 +0800

    update

--- config/db.js
+++ config/db.js
@@ -1,33 +1,33 @@
-const sequelizeTemp = require('./local_db')
-const Sequelize = require('sequelize');
-let db = null
+const sequelizeTemp = require("./local_db");
+const Sequelize = require("sequelize");
+let db = null;
 if (sequelizeTemp) {
-  db = sequelizeTemp.sequelize
+  db = sequelizeTemp.sequelize;
 } else {
-  const sequelize = new Sequelize('monitor_db', 'username', 'password', {
-    host: 'ip',
-    dialect: 'mysql',
+  console.log(111);
+  const sequelize = new Sequelize("monitor_db", "username", "password", {
+    host: "ip",
+    dialect: "mysql",
     operatorsAliases: false,
     dialectOptions: {
       charset: "utf8mb4",
       collate: "utf8mb4_unicode_ci",
       supportBigNumbers: true,
-      bigNumberStrings: true
+      bigNumberStrings: true,
     },
 
     pool: {
       max: 30,
       min: 0,
       acquire: 30000,
-      idle: 10000
+      idle: 10000,
     },
-    timezone: '+08:00' //东八时区
+    timezone: "+08:00", //东八时区
   });
 
-  db = sequelize
-
+  db = sequelize;
 }
 
 module.exports = {
-  sequelize: db
-}
+  sequelize: db,
+};
